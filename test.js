a8da2b3f4f90dc61af4fd62aa49b6b4853d27a5f
Vineet Singh <viratsinghkaharwar8923@gmail.com>
2022-10-30 19:11:31 +0530

    new particle added and reduce memory use

--- test.js
+++ test.js
@@ -8,17 +8,19 @@ const color = () => {
   var color = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
   return color;
 };
-document.body.style.backgroundColor='#020202'
-let bg=document.querySelector('.bg')
-let span=document.querySelectorAll('.bg span')
-const layer=document.querySelector('.layer')
+document.body.style.backgroundColor = "#000";
+
+let bg = document.querySelector(".bg");
+for (let i = 1; i < 100; i++) {
+  const particles = document.createElement("span");
+  bg.appendChild(particles);
+}
+let span = document.querySelectorAll(".bg span");
+const layer = document.querySelector(".layer");
 setInterval(() => {
-  span.forEach((element,index)=>{
-    element.style.background=color();
-    // element.style.opacity=Math.random()
-    element.style.borderRadius=Math.floor(Math.random() * 0xff).toString(16)+'%'
-    element.style.rotate=Math.random() * 6 + 'deg';
-    element.style.filter=`drop-shadow(0 0 20px ${color()})`
-    element.style.rotate=`33deg`
-  })
+  span.forEach((element, index) => {
+    element.style.background = color();
+    element.style.borderRadius = Math.floor(Math.random() * 0xff).toString(16) + "%";
+    element.style.scale=Math.random()
+  });
 }, 1000);

69009b09aa882d9e904c5efbc42214c30ea8fe7a
vineet <viratsinghkaharwar8923@gmail.com>
2023-07-28 20:51:57 +0530

    fixed some bug

--- test.js
+++ test.js
@@ -1,26 +1,24 @@
-const msg = ["hello", "how are you"];
-// console.table(msg)
-const duplicate = "hello";
-const data = ["hello", "hello", duplicate];
-
-let real = new Set(data);
 const color = () => {
   var color = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
   return color;
 };
-document.body.style.backgroundColor = "#000";
-
 let bg = document.querySelector(".bg");
-for (let i = 1; i < 100; i++) {
-  const particles = document.createElement("span");
-  bg.appendChild(particles);
+for (let i = 1; i < window.innerWidth; i++, i += 100) {
+  for (let j = 1; j < window.innerHeight; j++, j += 100) {
+    let span = document.createElement("span");
+    span.style.background = color();
+    span.style.borderRadius =
+      Math.floor(Math.random() * 0xff).toString(16) + "%";
+    span.style.scale = Math.random();
+    bg.appendChild(span);
+  }
 }
 let span = document.querySelectorAll(".bg span");
-const layer = document.querySelector(".layer");
 setInterval(() => {
   span.forEach((element, index) => {
     element.style.background = color();
-    element.style.borderRadius = Math.floor(Math.random() * 0xff).toString(16) + "%";
-    element.style.scale=Math.random()
+    element.style.borderRadius =
+      Math.floor(Math.random() * 0xff).toString(16) + "%";
+    element.style.scale = Math.random();
   });
-}, 1000);
+}, 3000);
